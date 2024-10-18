f493e587a2837d5120ae9b393256c03dd014993e
mjapplitools <matt.jasaitis@applitools.com>
2024-10-18 14:59:30 -0700

    added cypress open snippet

---
+++ cypress/snippets/open.js
@@ -0,0 +1,9 @@
+cy.eyesOpen({
+    appName: 'ACME Bank',
+    testName: 'Cypress - UFG Test',
+    browser: [
+        {width: 800, height: 600, name: 'firefox'},
+        {width: 800, height: 600, name: 'chrome'},
+        {width: 800, height: 600, name: 'safari'}
+    ]
+})
